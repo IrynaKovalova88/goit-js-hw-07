3b2376e6ffe70a3a058d802e973136a11029664a
IrynaKovalova88 <Avril19882012@gmail.com>
2021-10-23 14:26:10 +0300

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const imageMarkup = createImageMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imageMarkup);
 
-console.log(galleryItems);
+function createImageMarkup(items) {
+return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a
+        class="gallery__link"
+        href = '${original}';
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+    `;
+    })
+    .join('');    
+};
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+    instance.element().querySelector('img').src = event.target.dataset.source;
+    instance.show();
+};
+
+const instance = basicLightbox.create(`<img src="" />`, {
+  onShow: () => {
+    window.addEventListener('keydown', keydownEscape);
+  },
+  onClose: () => {
+    window.removeEventListener('keydown', keydownEscape);
+  },
+});
+
+function keydownEscape(event) {
+  console.log(event);
+  if (event.key === 'Escape') {
+    instance.close();
+    return;
+  }
+};
